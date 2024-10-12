734dd59b8a3ee9fdc47a575cd7cb272f048626c5
Deepak Rizal <deepak2000rizal@gmail.com>
2024-10-12 19:10:12 +0530

    made necessary changes in  react-router router dom for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -8,32 +8,37 @@ import Layout from "./components/Layout.jsx";
 import Cart from "./components/Cart.jsx";
 import ProductDescription from "./components/ProductDescription.jsx";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: (
+        <Layout>
+          <App />
+        </Layout>
+      ),
+    },
+    {
+      path: "/cart",
+      element: (
+        <Layout>
+          <Cart />
+        </Layout>
+      ),
+    },
+    {
+      path: "/product/:id",
+      element: (
+        <Layout>
+          <ProductDescription />
+        </Layout>
+      ),
+    },
+  ],
   {
-    path: "/",
-    element: (
-      <Layout>
-        <App />
-      </Layout>
-    ),
-  },
-  {
-    path: "/cart",
-    element: (
-      <Layout>
-        <Cart />
-      </Layout>
-    ),
-  },
-  {
-    path: "/product/:id",
-    element: (
-      <Layout>
-        <ProductDescription />
-      </Layout>
-    ),
-  },
-]);
+    basename: "/Ecommerce-product-page-frontend",
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
