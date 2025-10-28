9d8236f7245f203fdf76426f268f907142c0883c
Deepak Rizal <deepak2000rizal@gmail.com>
2025-10-28 15:52:08 +0530

    fix: show number of items in cart icon correctly

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,6 +6,8 @@ import { CartContext } from "../features/ContextProvider";
 const Navigation = () => {
   const { cart } = useContext(CartContext);
 
+  const totalProduct = cart.reduce((acc, item) => (acc += item.quantity), 0);
+
   return (
     <div className="fixed top-0 left-0 w-full z-50 flex px-10 md:px-6 lg:px-10 py-4 justify-between bg-blue-900 text-white ">
       <Link to={"/"} className="font-bold ">
@@ -14,7 +16,7 @@ const Navigation = () => {
       <Link to={"/cart"} className=" relative ">
         {cart.length > 0 && (
           <span className="bg-orange-500 w-5 h-5 rounded-full text-xs absolute flex items-center justify-center top-[-10px] right-[-13px]">
-            {cart.length}
+            {totalProduct}
           </span>
         )}
         <ShoppingCartIcon />
